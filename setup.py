e651c56769ff50cdbdaf8258fa162a1bd9814b49
Jana-A <awada_jana@yahoo.com>
2014-11-23 22:30:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup (
 	long_description = """
 							-------\ LIS \--------
 							This module contains the main function "LIS" plus 2 ad hoc functions used by LIS namely "nest" and "gmls".
-							Given a list of unique numbers, the LIS function outputs all LIS from list i.e. the longest increasing subsequences having the maximum length among all results. To implement, type "from longest_increasing_subsequence import LIS" then try "LIS([2,5,1,3,4])". Note that the external module list_operations is required "from list_operations import unnest".
+							Given a list of unique numbers, the LIS function outputs all LIS from list i.e. the longest increasing subsequences having the maximum length among all results. To implement, type "from L_I_S import LIS" then try "LIS([2,5,1,3,4])". Note that the external module list_operations is required "from list_operations import unnest".
 					   """,
 	install_requires = ['list_operations.unnest'],
 	keywords = ['longest increasing subsequence'],
